6d5e7fd86e361bc03df3c0d6567a466cc2e864f9
AnqiYang <yangllzz01@163.com>
2021-03-27 16:02:37 -0400

    refractor

--- utils/myConv2.m
+++ utils/myConv2.m
@@ -1,20 +1,26 @@
-function imgBlur = myConv2(img, kernels)
+function imgBlur = myConv2(img, kernel, condition)
 
 Isize = size(img);
-ksize = size(kernels);
-sz = Isize - ksize + 1;
+ksize = size(kernel);
+sz = Isize + ksize - 1;
 
 % implement convolution
-imgBlur = zeros(Isize(1)*2, Isize(2)*2, 3);
-for cc = 1:3
-    kernel = kernels(:,:,cc);   % normalized done
-    K = fft2(kernel, Isize(1)*2, Isize(2)*2);
-    I = fft2(img(:,:,cc), Isize(1)*2, Isize(2)*2);
-    imgBlur(:,:,cc) = ifft2(K.*I);
-end
+imgBlur = zeros(sz(1), sz(2));
+%     kernel = kernels(:,:,cc);   % normalized done
+K = fft2(kernel, sz(1), sz(2));
+I = fft2(img, sz(1), sz(2));
+imgBlur = ifft2(K.*I);
 
-xx = floor(ksize(1)/2);
-yy = floor(ksize(2)/2);
-imgBlur = imgBlur(xx+1:xx+Isize(1), yy+1:yy+Isize(2), :);
+switch condition
+    case 'valid'
+        outSz = Isize - ksize + 1;
+        imgBlur = imgBlur(ksize(1)+1: outSz(1)+ksize(1),ksize(2)+1: outSz(2)+ksize(2));
+    case 'same'
+        xx = floor(ksize(1)/2);
+        yy = floor(ksize(2)/2);
+        imgBlur = imgBlur(xx+1:xx+Isize(1), yy+1:yy+Isize(2));
+    case 'full'
+        imgBlur = imgBlur;            
+end
 
 end
